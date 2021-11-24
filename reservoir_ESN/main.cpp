9d758dce3813e08a1e1d615715d0d1eca6dcc468
ryoutarou0118 <ryoutarou2000@outlook.jp>
2021-11-24 16:17:15 +0900

    エノンマップ5 ノード50までデータ生成

--- reservoir_ESN/main.cpp
+++ reservoir_ESN/main.cpp
@@ -16,10 +16,10 @@
 #define MAX_NODE_SIZE (500)
 //非線形カーネル　関数の選択　いまのところマッキーグラスのみを想定
 double TDE_MG(const double x, double J, double input_gain, double feed_gain) {//Mackey_Glass
-	return (feed_gain * (x + input_gain * J)) / (1.0 + pow(x + input_gain * J, 3.0));//ρ = 2-------------------------
+	return (feed_gain * (x + input_gain * J)) / (1.0 + pow(x + input_gain * J, 6.0));//ρ = 2-------------------------
 }
 double TDE_ikeda(const double x, double J, double input_gain, double feed_gain) {
-	return feed_gain * pow(sin(x + input_gain * J + 0.35), 2.0);
+	return feed_gain * pow(sin(x + input_gain * J + 0.4), 2.0);
 }
 
 double tanh(const double x, double J, double input_gain, double feed_gain) {
@@ -60,11 +60,11 @@ int main(void) {
 	const int TRIAL_NUM = 3;	
 	const int step = 3000;
 	const int wash_out = 500; 
-	std::vector<int> unit_sizes = { 20 };
+	std::vector<int> unit_sizes = { 50 };
 
-	std::vector<std::string> task_names = { "narma"};
+	std::vector<std::string> task_names = { "henon"};
 	if (unit_sizes.size() != task_names.size()) return 0;
-	std::vector<int> param1 = { 20 };
+	std::vector<int> param1 = { 5 };
 	std::vector<double> param2 = { 0};
 	if (param1.size() != param2.size()) return 0;
 	const int alpha_step = 11;
@@ -81,7 +81,7 @@ int main(void) {
 		const std::string task_name = task_names[r];
 		std::vector<std::vector<double>> input_signal(PHASE_NUM), teacher_signal(PHASE_NUM);
 
-		std::vector<std::string> function_names = { "TDE_MG", "TDE_ikeda",            };// "tanh", "sinc"は時間あれば
+		std::vector<std::string> function_names = { "TDE_MG", "TDE_ikeda",    };// "tanh", "sinc"は時間あれば
 		double alpha_min, d_alpha;//タスクによって最小値が変わる　
 		double sigma_min, d_sigma;
 		double d_bias;
@@ -93,7 +93,7 @@ int main(void) {
 				d_bias = 0.2;
 				//d_alpha = 0.05; alpha_min = 0.10; 現状これ(NARMA10も含めると)
 				//d_alpha = 0.05; alpha_min = 0.80;NARMA5に限ってはこっち
-				d_alpha = 0.02; alpha_min = 0.4;
+				d_alpha = 0.05; alpha_min = 0.1;
 				//d_alpha = 0.02; alpha_min = 0.4;
 				//d_sigma = 0.07; sigma_min = 0.4;
 				const int tau = param1[r];
@@ -110,9 +110,18 @@ int main(void) {
 				generate_narma_task2(input_signal[phase], teacher_signal[phase], tau, step);
 			}
 			else if (task_name == "henon") {
+				/*int a = 0;
+				auto function_name = function_names;
+				if (function_name[a] == "TDE_MG") {
+						d_alpha = 0.2; alpha_min = 0.0;
+				}
+				else if (function_name[a] == "TDE_ikeda") {
+					d_alpha = 2.0; alpha_min = 0.0;
+				}*/
+
 
 				//d_bias = 1.0;
-				d_alpha = 1.0; alpha_min = 1.0;
+				//d_alpha = 0.2; alpha_min = 0.0;
 				//d_sigma = 0.04; sigma_min = 0.04;
 				const int fstep = param1[r];
 				generate_henom_map_task(input_signal[phase], teacher_signal[phase], fstep, step, phase * step);
@@ -121,7 +130,7 @@ int main(void) {
 				//このタスクでは、サンタフェ時系列競争の混沌とした時系列の一歩先の予測をおこなう
 				// とくにカオス領域で動作する赤外線レーザーによって作成されたデータセット1万ポイントの継続ファイルを使用
 				d_bias = 0.5;
-				d_alpha = 0.4; alpha_min = 0.1;
+				d_alpha = 2.0; alpha_min = 0.1;
 				d_sigma = 0.1; sigma_min = 0.1;
 				const int fstep = param1[r];
 				generate_laser_task(input_signal[phase], teacher_signal[phase], fstep, step, phase * step);
@@ -174,12 +183,18 @@ int main(void) {
 		for (auto function_name : function_names) {
 			//double (*nonlinear)(double);//変更
 			double (*nonlinear)(double, double, double, double);
-			if (function_name == "TDE_MG") nonlinear = TDE_MG;
+			if (function_name == "TDE_MG") {
+				nonlinear = TDE_MG;
+				d_alpha = 0.2; alpha_min = 3.0;
+			}
 			else if (function_name == "tanh") nonlinear = tanh;
 			//else if (function_name == "gauss") nonlinear = gauss;
 			//else if (function_name == "oddsinc") nonlinear = oddsinc;
 			else if (function_name == "sinc") nonlinear = sinc;
-			else if (function_name == "TDE_ikeda") nonlinear = TDE_ikeda;
+			else if (function_name == "TDE_ikeda") {
+				nonlinear = TDE_ikeda;
+				d_alpha = 0.4; alpha_min = 8.0;
+			}
 			else if (function_name == "TDE_exp") nonlinear = TDE_exp;
 			else {
 				std::cerr << "error! " << function_name << "is not found" << std::endl;
@@ -205,21 +220,21 @@ int main(void) {
 					//outputfile2 << "function_name,input_gain,feed_gain,opt_nmse" << std::endl;
 					for (int ite_input = 1; ite_input <= 10; ite_input += 1) {//入力ゲイン(τ = 95 pa = 2 ノード100の時は 1～1.3付近で最適なリザバーが出来上がっていた(あと、NARMAタスク, d_bias = 0.4 d_alpha = 0.05, d_sigma = 0.07))
 						//const double input_gain = d_bias * ite_input * 0.1;//d_biasの部分たぶん無くす　
-						//const double input_gain = 0.6 + ite_input * 0.05;
+						//const double input_gain = 0.7 + ite_input * 0.05;
 						//NARMA10の場合300秒かけた結果、入力ゲインが0.25, フィードゲインが0.9の時に0.16418というNMSEを達成
 						//const double input_gain = 0.45 + ite_input * 0.02;
-						//const double input_gain = 0.0 + ite_input * 0.2;
+						//const double input_gain = 0.0 + ite_input * 0.1;
 				 
-						const double input_gain = 0.2 + ite_input * 0.02;
-						//const double input_gain = 0.7 + ite_input * 0.04;
+						//const double input_gain = 0.0 + ite_input * 0.02;
+						const double input_gain = 0.7 + ite_input * 0.04;
 						for (int ite_feed = 1; ite_feed <= 10; ite_feed += 1) {//τ = 95 pa = 2 ノード100の時は 0.35で最適なリザバーが出来上がることが多かった
 							//double opt_nmse = 1e+10;
 							//const double feed_gain = d_bias * ite_feed / 20.0;//d_biasの部分無くす、もしくは変更する--  フィードバックゲインパラメーターηを1から3の間で変化させます。すでに説明したように、自律領域のTDRは、これらのパラメーター値に対して、±（η- 1）1/2;
-							const double feed_gain = 0.72 + ite_feed * 0.04;
-							//const double feed_gain = 0.65 + ite_feed * 0.05;
+							//const double feed_gain = 0.72 + ite_feed * 0.04;
+							//const double feed_gain = 0.65 + ite_feed * 0.04;
 							//const double feed_gain = 0.8 + ite_feed * 0.02;
 							//const double feed_gain = 0.2 + ite_feed * 0.1;
-							//const double feed_gain = 0.2 + ite_feed * 0.02;
+							const double feed_gain = 0.2 + ite_feed * 0.02;
 #pragma omp parallel for num_threads(32)
 						// 複数のリザーバーの時間発展をまとめて処理
 							for (int k = 0; k < alpha_step; k++) {
