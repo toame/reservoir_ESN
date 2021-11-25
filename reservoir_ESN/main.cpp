63bcfb0b6fa98236a20784df107fbd80d0e1348f
ryoutarou0118 <ryoutarou2000@outlook.jp>
2021-11-25 18:58:32 +0900

    a

--- reservoir_ESN/main.cpp
+++ reservoir_ESN/main.cpp
@@ -60,7 +60,7 @@ int main(void) {
 	const int TRIAL_NUM = 3;	
 	const int step = 3000;
 	const int wash_out = 500; 
-	std::vector<int> unit_sizes = { 300 };
+	std::vector<int> unit_sizes = { 20 };
 
 	std::vector<std::string> task_names = { "henon"};
 	if (unit_sizes.size() != task_names.size()) return 0;
