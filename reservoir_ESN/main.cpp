00fd4b1deb94e3125b7a4509689640823aa72ae9
ryoutarou0118 <ryoutarou2000@outlook.jp>
2021-11-26 09:59:07 +0900

    関数近似τ5, ν1.5 データ生成

--- reservoir_ESN/main.cpp
+++ reservoir_ESN/main.cpp
@@ -19,7 +19,7 @@ double TDE_MG(const double x, double J, double input_gain, double feed_gain) {//
 	return (feed_gain * (x + input_gain * J)) / (1.0 + pow(x + input_gain * J, 8.0));//ρ = 2-------------------------
 }
 double TDE_ikeda(const double x, double J, double input_gain, double feed_gain) {
-	return feed_gain * pow(sin(x + input_gain * J + 0.3), 2.0);
+	return feed_gain * pow(sin(x + input_gain * J + 0.6), 2.0);
 }
 
 double tanh(const double x, double J, double input_gain, double feed_gain) {
@@ -60,7 +60,7 @@ int main(void) {
 	const int TRIAL_NUM = 3;	
 	const int step = 3000;
 	const int wash_out = 500; 
-	std::vector<int> unit_sizes = { 20 };
+	std::vector<int> unit_sizes = { 50 };
 
 	std::vector<std::string> task_names = { "approx"};
 	if (unit_sizes.size() != task_names.size()) return 0;
@@ -81,7 +81,7 @@ int main(void) {
 		const std::string task_name = task_names[r];
 		std::vector<std::vector<double>> input_signal(PHASE_NUM), teacher_signal(PHASE_NUM);
 
-		std::vector<std::string> function_names = { "TDE_MG",  "TDE_ikeda",           };// "tanh", "sinc"は時間あれば
+		std::vector<std::string> function_names = { "TDE_MG", "TDE_ikeda",               };// "tanh", "sinc"は時間あれば
 		double alpha_min, d_alpha;//タスクによって最小値が変わる　
 		double sigma_min, d_sigma;
 		double d_bias;
@@ -193,7 +193,7 @@ int main(void) {
 			else if (function_name == "sinc") nonlinear = sinc;
 			else if (function_name == "TDE_ikeda") {
 				nonlinear = TDE_ikeda;
-				d_alpha = 1.0; alpha_min = 10.0;
+				d_alpha = 1.0; alpha_min = 1.0;
 			}
 			else if (function_name == "TDE_exp") nonlinear = TDE_exp;
 			else {
@@ -202,7 +202,7 @@ int main(void) {
 			}
 
 			for (int loop = 0; loop < TRIAL_NUM; loop++) {//論文 p12 ばらつき低減
-				for (int ite_p = 0; ite_p <= 10; ite_p += 1) {//論文　手順２
+				for (int ite_p = 1; ite_p <= 2; ite_p += 1) {//論文　手順２
 					const double p = ite_p * 0.1;
 					double opt_nmse = 1e+10;//opt 最適な値  
 					double opt_input_signal_factor = 0;
@@ -222,19 +222,19 @@ int main(void) {
 						//const double input_gain = d_bias * ite_input * 0.1;//d_biasの部分たぶん無くす　
 						//const double input_gain = 0.8 + ite_input * 0.05;
 						//NARMA10の場合300秒かけた結果、入力ゲインが0.25, フィードゲインが0.9の時に0.16418というNMSEを達成
-						const double input_gain = 0.4 + ite_input * 0.1;
+						//const double input_gain = 0.4 + ite_input * 0.05;
 						//const double input_gain = 0.1 + ite_input * 0.1;
 				 
-						//const double input_gain = 0.2 + ite_input * 0.02;
+						const double input_gain = 0.2 + ite_input * 0.02;
 						//const double input_gain = 0.7 + ite_input * 0.04;
 						for (int ite_feed = 1; ite_feed <= 10; ite_feed += 1) {//τ = 95 pa = 2 ノード100の時は 0.35で最適なリザバーが出来上がることが多かった
 							//double opt_nmse = 1e+10;
 							//const double feed_gain = d_bias * ite_feed / 20.0;//d_biasの部分無くす、もしくは変更する--  フィードバックゲインパラメーターηを1から3の間で変化させます。すでに説明したように、自律領域のTDRは、これらのパラメーター値に対して、±（η- 1）1/2;
 							//const double feed_gain = 0.72 + ite_feed * 0.04;
-							//const double feed_gain = 0.65 + ite_feed * 0.04;
-							//const double feed_gain = 0.8 + ite_feed * 0.02;
+							const double feed_gain = 0.5 + ite_feed * 0.05;
+						   // const double feed_gain = 0.8 + ite_feed * 0.02;
 							//const double feed_gain = 0.1 + ite_feed * 0.1;
-							const double feed_gain = 0.2 + ite_feed * 0.02;
+							//const double feed_gain = 0.2 + ite_feed * 0.05;
 #pragma omp parallel for num_threads(32)
 						// 複数のリザーバーの時間発展をまとめて処理
 							for (int k = 0; k < alpha_step; k++) {
