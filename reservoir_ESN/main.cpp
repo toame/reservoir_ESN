55739515b0b9b2bf9b8b0c2a2872c7bc3ba0e99b
ryoutarou0118 <ryoutarou2000@outlook.jp>
2021-11-26 17:56:49 +0900

    関数近似τ5, ν1.5 ノード300　データ生成

--- reservoir_ESN/main.cpp
+++ reservoir_ESN/main.cpp
@@ -19,7 +19,7 @@ double TDE_MG(const double x, double J, double input_gain, double feed_gain) {//
 	return (feed_gain * (x + input_gain * J)) / (1.0 + pow(x + input_gain * J, 8.0));//ρ = 2-------------------------
 }
 double TDE_ikeda(const double x, double J, double input_gain, double feed_gain) {
-	return feed_gain * pow(sin(x + input_gain * J + 0.35), 2.0);
+	return feed_gain * pow(sin(x + input_gain * J + 0.45), 2.0);
 }
 
 double tanh(const double x, double J, double input_gain, double feed_gain) {
@@ -60,12 +60,12 @@ int main(void) {
 	const int TRIAL_NUM = 3;	
 	const int step = 3000;
 	const int wash_out = 500; 
-	std::vector<int> unit_sizes = { 100 };
+	std::vector<int> unit_sizes = { 20 };
 
 	std::vector<std::string> task_names = { "approx"};
 	if (unit_sizes.size() != task_names.size()) return 0;
-	std::vector<int> param1 = { 5 };
-	std::vector<double> param2 = { 1.5};
+	std::vector<int> param1 = { 2 };
+	std::vector<double> param2 = { 5.0};
 	if (param1.size() != param2.size()) return 0;
 	const int alpha_step = 11;
 	const int sigma_step = 11;
@@ -81,7 +81,7 @@ int main(void) {
 		const std::string task_name = task_names[r];
 		std::vector<std::vector<double>> input_signal(PHASE_NUM), teacher_signal(PHASE_NUM);
 
-		std::vector<std::string> function_names = { "TDE_MG", "TDE_ikeda",                 };// "tanh", "sinc"は時間あれば
+		std::vector<std::string> function_names = { "TDE_MG", "TDE_ikeda",                       };//  "sinc"は時間あれば
 		double alpha_min, d_alpha;//タスクによって最小値が変わる　
 		double sigma_min, d_sigma;
 		double d_bias;
@@ -185,24 +185,30 @@ int main(void) {
 			double (*nonlinear)(double, double, double, double);
 			if (function_name == "TDE_MG") {
 				nonlinear = TDE_MG;
-				d_alpha = 0.4; alpha_min = 6.0;
+				d_alpha = 0.5; alpha_min = 10.0;
 			}
-			else if (function_name == "tanh") nonlinear = tanh;
+			else if (function_name == "tanh") {
+				d_alpha = 0.2; alpha_min = 15.0;
+				nonlinear = tanh;
+			}
 			//else if (function_name == "gauss") nonlinear = gauss;
 			//else if (function_name == "oddsinc") nonlinear = oddsinc;
 			else if (function_name == "sinc") nonlinear = sinc;
 			else if (function_name == "TDE_ikeda") {
 				nonlinear = TDE_ikeda;
-				d_alpha = 1.0; alpha_min = 6.0;
+				d_alpha = 0.2; alpha_min = 15.0;
 			}
-			else if (function_name == "TDE_exp") nonlinear = TDE_exp;
+			else if (function_name == "TDE_exp") {
+				nonlinear = TDE_exp;
+				d_alpha = 0.2; alpha_min = 15.0;
+			}
 			else {
 				std::cerr << "error! " << function_name << "is not found" << std::endl;
 				return 0;
 			}
 
-			for (int loop = 0; loop < TRIAL_NUM; loop++) {//論文 p12 ばらつき低減
-				for (int ite_p = 0; ite_p <= 10; ite_p += 1) {//論文　手順２
+			for (int loop = 0; loop < 1; loop++) {//論文 p12 ばらつき低減
+				for (int ite_p = 1; ite_p <= 3; ite_p += 1) {//論文　手順２
 					const double p = ite_p * 0.1;
 					double opt_nmse = 1e+10;//opt 最適な値  
 					double opt_input_signal_factor = 0;
@@ -225,16 +231,16 @@ int main(void) {
 						//const double input_gain = 0.5 + ite_input * 0.05;
 						//const double input_gain = 0.1 + ite_input * 0.1;
 				 
-						const double input_gain = 0.2 + ite_input * 0.02;
-						//const double input_gain = 0.7 + ite_input * 0.04;
+						const double input_gain = 0.55 + ite_input * 0.05;
+						//const double input_gain = 0.7 + ite_input * 0.04;こっち
 						for (int ite_feed = 1; ite_feed <= 10; ite_feed += 1) {//τ = 95 pa = 2 ノード100の時は 0.35で最適なリザバーが出来上がることが多かった
 							//double opt_nmse = 1e+10;
 							//const double feed_gain = d_bias * ite_feed / 20.0;//d_biasの部分無くす、もしくは変更する--  フィードバックゲインパラメーターηを1から3の間で変化させます。すでに説明したように、自律領域のTDRは、これらのパラメーター値に対して、±（η- 1）1/2;
 							//const double feed_gain = 0.72 + ite_feed * 0.04;
-							const double feed_gain = 0.5 + ite_feed * 0.05;
-						   // const double feed_gain = 0.8 + ite_feed * 0.02;
-							//const double feed_gain = 0.1 + ite_feed * 0.1;
-							//const double feed_gain = 0.2 + ite_feed * 0.02;
+							//const double feed_gain = 0.5 + ite_feed * 0.05;
+						    //const double feed_gain = 0.8 + ite_feed * 0.04;
+							const double feed_gain = 0.1 + ite_feed * 0.1;
+							//const double feed_gain = 0.1 + ite_feed * 0.02;こっち
 #pragma omp parallel for num_threads(32)
 						// 複数のリザーバーの時間発展をまとめて処理
 							for (int k = 0; k < alpha_step; k++) {
