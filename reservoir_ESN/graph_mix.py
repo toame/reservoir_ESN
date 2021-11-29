b9626406740c8dfca6a3a85ac0b238e33c75b474
ryoutarou0118 <ryoutarou2000@outlook.jp>
2021-11-29 16:00:48 +0900

    比較グラフ　完成

--- reservoir_ESN/graph_mix.py
+++ reservoir_ESN/graph_mix.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import os
 from matplotlib import pyplot as plt
-folder = "output_data_mix/"
+folder = "Various_ESN_comparison/"
 for name in os.listdir(folder):
     #name = "approx_3_3.0_100"
     name = os.path.splitext(os.path.basename(name))[0]
     df = pd.read_csv(folder + name + '.txt', sep=',',comment='#')
     p_list = df["p"].unique()
-    function_names = ["mackey", "sinc", "oddsinc", "gauss"];
+    function_names = ["TD_MG", "TD_ikeda","RND_sinc"];
     for function_name in function_names:
         data_x, data_y = [], []
         for p in p_list:
