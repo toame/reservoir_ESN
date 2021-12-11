013307079b6ba8d40a5680074c3b2d6e44f3c206
ryoutarou0118 <ryoutarou2000@outlook.jp>
2021-12-11 10:56:19 +0900

    あ

--- reservoir_ESN/main.cpp
+++ reservoir_ESN/main.cpp
@@ -16,10 +16,10 @@
 #define MAX_NODE_SIZE (500)
 //非線形カーネル　関数の選択　いまのところマッキーグラスのみを想定
 double TD_MG(const double x, double J, double input_gain, double feed_gain) {//Mackey_Glass
-	return (feed_gain * (x + input_gain * J)) / (1.0 + pow(x + input_gain * J, 8.0));//ρ = 2-------------------------
+	return (feed_gain * (x + input_gain * J)) / (1.0 + pow(x + input_gain * J, 6.0));//ρ = 2-------------------------
 }
 double TD_ikeda(const double x, double J, double input_gain, double feed_gain) {
-	return feed_gain * pow(sin(x + input_gain * J + 0.50), 2.0);
+	return feed_gain * pow(sin(x + input_gain * J + 0.6), 2.0);
 }
 
 double tanh(const double x, double J, double input_gain, double feed_gain) {
@@ -62,10 +62,10 @@ int main(void) {
 	const int wash_out = 500; 
 	std::vector<int> unit_sizes = { 20 };
 
-	std::vector<std::string> task_names = { "henon"};
+	std::vector<std::string> task_names = { "approx"};
 	if (unit_sizes.size() != task_names.size()) return 0;
 	std::vector<int> param1 = { 3 };
-	std::vector<double> param2 = { 0.0};
+	std::vector<double> param2 = { 1.5};
 	if (param1.size() != param2.size()) return 0;
 	const int alpha_step = 11;
 	const int sigma_step = 11;
@@ -203,7 +203,7 @@ int main(void) {
 			double (*nonlinear)(double, double, double, double);
 			if (function_name == "TD_MG") {
 				nonlinear = TD_MG;
-				d_alpha = 0.2; alpha_min = 1.0;
+				d_alpha = 0.4; alpha_min = 2.0;
 			}
 			else if (function_name == "tanh") {
 				//d_alpha = 0.2; alpha_min = 0.6;
@@ -214,7 +214,7 @@ int main(void) {
 			else if (function_name == "sinc") nonlinear = sinc;
 			else if (function_name == "TD_ikeda") {
 				nonlinear = TD_ikeda;
-				d_alpha = 0.3; alpha_min = 3.0;
+				d_alpha = 0.2; alpha_min = 2.0;
 			}
 			else if (function_name == "STDE_exp") {
 				nonlinear = STDE_exp;
@@ -246,9 +246,9 @@ int main(void) {
 						//const double input_gain = d_bias * ite_input * 0.1;//d_biasの部分たぶん無くす　
 						//const double input_gain = 1.0 + ite_input * 0.05;
 						//NARMA10の場合300秒かけた結果、入力ゲインが0.25, フィードゲインが0.9の時に0.16418というNMSEを達成
-						//const double input_gain = 0.2 + ite_input * 0.02;
+						//const double input_gain = 0.2 + ite_input * 0.04;
 						//const double input_gain = 0.1 + ite_input * 0.1;
-						//const double input_gain = 0.6 + ite_input * 0.05;
+						//const double input_gain = 0.5 + ite_input * 0.05;
 						//const double input_gain = 0.0 + ite_input * 0.05;
 						const double input_gain = 0.7 + ite_input * 0.04;
 						for (int ite_feed = 1; ite_feed <= 10; ite_feed += 1) {//τ = 95 pa = 2 ノード100の時は 0.35で最適なリザバーが出来上がることが多かった
