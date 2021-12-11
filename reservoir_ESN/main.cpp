947b7bf042fe349771fe9cbf602542fd94c1936b
ryoutarou0118 <ryoutarou2000@outlook.jp>
2021-12-11 09:31:59 +0900

    関数近似　τ=3, ν=1.5 データ生成

--- reservoir_ESN/main.cpp
+++ reservoir_ESN/main.cpp
@@ -41,16 +41,16 @@ int main(void) {
 	std::vector<int> unit_sizes = { 20, // 20,  20,  20,  
 									/*100, 100, 100,  100, 100,  100, 100, 100, 100,  100, 100, 100, 100,  100, 100, 100,
 									200, 200, 200,  200, 200,  200, 200, 200, 200,  200, 200, 200, 200,  200, 200, 200*/ };
-	std::vector<std::string> task_names = { "henon",//  "narma", "laser", "approx",
+	std::vector<std::string> task_names = { "approx",// "henon",  "narma", "laser",
 
 											/* "approx","narma", "narma", "narma", "narma", "narma2", "narma2", "narma2", "narma2", "approx", "approx", "approx","laser", "laser", "laser", "henon", //"henon",
 											"narma", "narma", "narma", "narma", "narma2", "narma2", "narma2", "narma2", "approx", "approx", "approx",  "laser", "laser", "laser", "henon", "henon",*/ };
 	if (unit_sizes.size() != task_names.size()) return 0;
-	std::vector<int> param1 = { 3,//  5,  7,  7,
+	std::vector<int> param1 = {3,// 3,  5,  7, 
 
 								  /* 3, 10, 5, 15, 20, 5, 10, 15, 20, 3, 5, 7, 1, 3, 10, 5,// 7,
 									  5, 10, 15, 20, 5, 10, 15, 20, 3, 5, 7,  1, 3, 10, 5, 7*/ };
-	std::vector<double> param2 = { 0.0,//  0.0,  0.0, 1.0,
+	std::vector<double> param2 = {1.5 //0.0,  0.0,  0.0,
 									/* 5.0, 0, 0, 0, 0,    0, 0,  0, 0,   3.0, 1.5, 1.0,  0, 0, 0,  0,// 0,
 								  0, 0, 0, 0,    0, 0,  0, 0,   3.0, 1.5, 1.0,  	0, 0, 0,  0, 0*/ };
 	if (param1.size() != param2.size()) return 0;
