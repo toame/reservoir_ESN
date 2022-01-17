56ab459e6972691aa3f2236077d769d24c09b873
ryoutarou0118 <ryoutarou2000@outlook.jp>
2022-01-17 11:19:11 +0900

    a

--- reservoir_ESN/graph.py
+++ reservoir_ESN/graph.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import os
 from matplotlib import pyplot as plt
-folder = "practice/"
+folder = "output_data_STDE/"
 for name in os.listdir(folder):
     #name = "approx_3_3.0_100"
     name = os.path.splitext(os.path.basename(name))[0]
     df = pd.read_csv(folder + name + '.txt', sep=',',comment='#')
     p_list = df["p"].unique()
-    function_names = ["TD_MG", "TDE_ikeda","TDE_exp"];
+    function_names = ["STD_MG", "STD_ikeda","TD_MG", "TD_ikeda"];
     for function_name in function_names:
         data_x, data_y = [], []
         for p in p_list:
