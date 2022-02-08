083040fc10b87be04e4065936d1cc2b5b16f01de
ryoutarou0118 <ryoutarou2000@outlook.jp>
2022-02-08 14:20:24 +0900

    プログラムまとめる2

--- reservoir_ESN/main.cpp
+++ reservoir_ESN/main.cpp
@@ -15,23 +15,14 @@
 #define TEST (2)
 #define MAX_NODE_SIZE (500)
 double TD_MG(const double x, double J, double input_gain, double feed_gain) {
-	return (feed_gain * (x + input_gain * J)) / (1.0 + pow(x + input_gain * J, 4.0));//ρ = 1～10
+	return (feed_gain * (x + input_gain * J)) / (1.0 + pow(x + input_gain * J, 4.0));//ρ = 1～10  //////////////変更要素/////////////////
 }
+
 double TD_ikeda(const double x, double J, double input_gain, double feed_gain) {
-	return feed_gain * pow(sin(x + input_gain * J + 0.5), 2.0);
+	return feed_gain * pow(sin(x + input_gain * J + 0.5), 2.0);//Φ=0.2～0.7(0.05刻み)  //////////////変更要素/////////////////
 }
 
-/*
-double tanh(const double x, double J, double input_gain, double feed_gain) {
-	return feed_gain * tanh(x + input_gain * J);
-}
-
-double sinc(const double x, double J, double input_gain, double feed_gain) {
-	if (x == 0) return 1.0;
-	return feed_gain * (sin(PI * (x + input_gain * J)) / (PI * (x + input_gain * J)));
-}*/
-
-double STDE_exp(const double x, double J, double input_gain, double feed_gain) {
+double TD_exp(const double x, double J, double input_gain, double feed_gain) {
 	return feed_gain * exp(-x) * sin(x + input_gain * J);
 }
 
@@ -52,12 +43,12 @@ int main(void) {
 	const int TRIAL_NUM = 3;	
 	const int step = 3000;
 	const int wash_out = 500; 
-	std::vector<int> unit_sizes = { 20 };
+	std::vector<int> unit_sizes = { 20 };///////////////////変更要素////////////////////////
 
-	std::vector<std::string> task_names = { "henon"};
+	std::vector<std::string> task_names = { "henon"};///////////////////変更要素////////////////////////
 	if (unit_sizes.size() != task_names.size()) return 0;
-	std::vector<int> param1 = { 5 };
-	std::vector<double> param2 = {0.0};
+	std::vector<int> param1 = { 5 };///////////////////変更要素////////////////////////
+	std::vector<double> param2 = {0.0};///////////////////変更要素////////////////////////
 	if (param1.size() != param2.size()) return 0;
 	const int alpha_step = 11;
 	const int sigma_step = 11;
@@ -73,7 +64,7 @@ int main(void) {
 		const std::string task_name = task_names[r];
 		std::vector<std::vector<double>> input_signal(PHASE_NUM), teacher_signal(PHASE_NUM);
 
-		std::vector<std::string> function_names = { "TD_MG",  "TD_ikeda", };// "STDE_MG", "STDE_ikeda", "STDE_exp"
+		std::vector<std::string> function_names = { "TD_MG",  "TD_ikeda", };// "STDE_MG", "STDE_ikeda", "TD_exp"  ///////////////////変更要素////////////////////////
 		double alpha_min, d_alpha;
 		double sigma_min, d_sigma;
 		double d_bias;
@@ -82,41 +73,41 @@ int main(void) {
 		for (int phase = 0; phase < PHASE_NUM; phase++) {
 			
 			if (task_name == "narma") {
-				d_alpha = 0.01; alpha_min = 0.1;
+				//d_alpha = 0.01; alpha_min = 0.1;
 				const int tau = param1[r];
 				generate_input_signal_random(input_signal[phase], -1.0, 2.0, step, phase + 1);
 				generate_narma_task(input_signal[phase], teacher_signal[phase], tau, step);
 			}
 			// 入力分布[-1, 1] -> 出力分布[0, 0.5]のnarmaタスク
 			else if (task_name == "narma2") {
-				d_alpha = 0.005; alpha_min = 0.002;
+				//d_alpha = 0.005; alpha_min = 0.002;
 				const int tau = param1[r];
 				generate_input_signal_random(input_signal[phase], -1.0, 2.0, step, phase + 1);
 				generate_narma_task2(input_signal[phase], teacher_signal[phase], tau, step);
 			}
 			else if (task_name == "henon") {
-				d_alpha = 0.2; alpha_min = 0.0;
+				//d_alpha = 0.2; alpha_min = 0.0;
 				const int fstep = param1[r];
 				generate_henom_map_task(input_signal[phase], teacher_signal[phase], fstep, step, phase * step);
 			}
 			else if (task_name == "henon2") {
-				d_alpha = 0.2; alpha_min = 0.0;
+				//d_alpha = 0.2; alpha_min = 0.0;
 				const int fstep = param1[r];
 				generate_henom_map_task2(input_signal[phase], teacher_signal[phase], fstep, step, phase * step);
 			}
 			else if (task_name == "laser") {
-				d_alpha = 2.0; alpha_min = 0.1;
+				//d_alpha = 2.0; alpha_min = 0.1;
 				const int fstep = param1[r];
 				generate_laser_task(input_signal[phase], teacher_signal[phase], fstep, step, phase * step);
 			}
 			else if (task_name == "approx") {
 				const int tau = param1[r];
 				const double nu = param2[r];
-				/*if (tau == 7) { d_alpha = 1.0; alpha_min = 0.1; d_bias = 0.5; d_sigma = 0.03; sigma_min = 0.1; }
-				else if (tau == 5) { d_alpha = 2.0; alpha_min = 0.5; d_bias = 1.0; d_sigma = 0.02; sigma_min = 0.02; }
-				else if (tau == 3) { d_alpha = 5.0; alpha_min = 1.0; d_bias = 4.0;  d_sigma = 0.02; sigma_min = 0.02; }
+				/*if (tau == 7) { d_alpha = 1.0; alpha_min = 0.1;}
+				else if (tau == 5) { d_alpha = 2.0; alpha_min = 0.5;}
+				else if (tau == 3) { d_alpha = 5.0; alpha_min = 1.0;}
 				else if (tau == 1) {
-					d_alpha = 10.0; alpha_min = 1.0; d_bias = 20.0;  d_sigma = 0.02; sigma_min = 0.02;
+					d_alpha = 10.0; alpha_min = 1.0;
 				}
 				else {
 					std::cerr << "error! approx parameter is not setting" << std::endl;
@@ -129,13 +120,13 @@ int main(void) {
 			else if (task_name == "approx2") {
 				const int tau = param1[r];
 				const double nu = param2[r];
-				//if (tau == 7) { d_alpha = 1.0; alpha_min = 0.1; d_bias = 0.5; d_sigma = 0.03; sigma_min = 0.1; }
-				//else if (tau == 5) { d_alpha = 2.0; alpha_min = 0.5; d_bias = 1.0; d_sigma = 0.02; sigma_min = 0.02; }
+				//if (tau == 7) { d_alpha = 1.0; alpha_min = 0.1;}
+				//else if (tau == 5) { d_alpha = 2.0; alpha_min = 0.5;}
 				//else if (tau == 3 || tau == 2) { 
-				//	//d_alpha = 1.0; alpha_min = 1.0; d_bias = 4.0;  d_sigma = 0.05; sigma_min = 0.05;
+				//	//d_alpha = 1.0; alpha_min = 1.0;
 				//}
 				//else if (tau == 1) {
-				//	d_alpha = 10.0; alpha_min = 1.0; d_bias = 20.0;  d_sigma = 0.02; sigma_min = 0.02;
+				//	d_alpha = 10.0; alpha_min = 1.0;
 				//}
 				//else {
 				//	std::cerr << "error! approx parameter is not setting" << std::endl;
@@ -145,6 +136,7 @@ int main(void) {
 				generate_input_signal_random(input_signal[phase], -1.0, 2.0, step, phase + 1);
 				task_for_function_approximation2(input_signal[phase], teacher_signal[phase], nu, tau, step, phase);
 			}
+			/*
 			else if (task_name == "legendre") {
 				const int tau = param1[r];
 				const double nu = param2[r];
@@ -161,7 +153,7 @@ int main(void) {
 
 				generate_input_signal_random(input_signal[phase], -1.0, 2.0, step, phase + 1);
 				generate_legendre_task(input_signal[phase], teacher_signal[phase], nu, tau, step);
-			}
+			}*/
 		}
 		// 設定出力
 		outputfile << "### task_name: " << task_name << std::endl;
@@ -176,15 +168,15 @@ int main(void) {
 			double (*nonlinear)(double, double, double, double);
 			if (function_name == "TD_MG") {
 				nonlinear = TD_MG;
-				d_alpha = 0.2; alpha_min = 0.0;
+				d_alpha = 0.2; alpha_min = 0.0;///////////////////変更要素////////////////////////
 			}
 			else if (function_name == "TD_ikeda") {
 				nonlinear = TD_ikeda;
-				d_alpha = 2.0; alpha_min = 0.0;
+				d_alpha = 2.0; alpha_min = 0.0;///////////////////変更要素////////////////////////
 			}
-			else if (function_name == "STDE_exp") {
-				nonlinear = STDE_exp;
-				//d_alpha = 0.05; alpha_min = 0.1;
+			else if (function_name == "TD_exp") {
+				nonlinear = TD_exp;
+				d_alpha = 0.05; alpha_min = 0.1;///////////////////変更要素////////////////////////
 			}
 			else {
 				std::cerr << "error! " << function_name << "is not found" << std::endl;
@@ -199,16 +191,16 @@ int main(void) {
 					//double opt_weight_factor = 0;
 					double opt_feed_gain = 0;//最適なフィードバックゲイン
 					double opt_input_gain = 0;//最適な入力ゲイン
-					double opt_lm2 = 0;//lmはλのこと
+					double opt_lm2 = 0;//lmはλ
 					double test_nmse = 1e+10;
 					double train_nmse = 1e+10;
 					reservoir_layer opt_reservoir_layer;
 					std::vector<double> opt_w;
 					start = std::chrono::system_clock::now(); // 計測開始時間
 					for (int ite_input = 1; ite_input <= 10; ite_input += 1) {
-						const double input_gain = 0.7 + ite_input * 0.05;
+						const double input_gain = 0.7 + ite_input * 0.05;///////////////////変更要素////////////////////////
 						for (int ite_feed = 1; ite_feed <= 10; ite_feed += 1) {
-							const double feed_gain = 0.10 + ite_feed * 0.02;
+							const double feed_gain = 0.10 + ite_feed * 0.02;//////////////////////変更要素/////////////////////////////
 #pragma omp parallel for num_threads(32)
 						// 複数のリザーバーの時間発展をまとめて処理
 							for (int k = 0; k < alpha_step; k++) {
